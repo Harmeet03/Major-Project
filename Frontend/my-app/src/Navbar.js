fcbc0baf15867701d2aab6d0e33512ab98973e83
Harmeet <102729017+Harmeet03@users.noreply.github.com>
2024-02-14 22:03:28 +0530

    Changes made

--- Frontend/my-app/src/Navbar.js
+++ Frontend/my-app/src/Navbar.js
@@ -2,67 +2,40 @@ import React from 'react';
 import './index';
 import {Link} from 'react-router-dom';
 
-const NavigationBar = () => {
+const Navbar = () => {
 return(
 <nav className='parent'>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
-<img id='b' src="https://esmarts.qodeinteractive.com/wp-content/uploads/2017/11/logo.png" alt='brandlogo'></img>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <img id='b' src="https://esmarts.qodeinteractive.com/wp-content/uploads/2017/11/logo.png" alt='brandlogo'></img>
 
-<div className="dropdown1">
-<button className="dropbtn">Home <i className="fa fa-caret-down"></i></button>
-<div className="dropdown-content">
-<Link to="/Rumala">Rumala Sahib</Link>
-<Link href="">Chattar Sahib</Link>
-<Link href="">Chaur Sahib</Link>
-</div>
-</div>
+    <div className="dropdown1">
+        <button className="dropbtn">Home <i className="fa fa-caret-down"></i></button>
+        <div className="dropdown-content">
+            <Link to="/Rumala">Rumala Sahib</Link>
+            <Link href="">Chattar Sahib</Link>
+            <Link href="">Chaur Sahib</Link>
+        </div>
+    </div>
 
-<div className="dropdown1">
-<button className="dropbtn">Courses <i className="fa fa-caret-down"></i></button>
-<div className="dropdown-content">
-<Link to="/Rumala">Rumala Sahib</Link>
-<Link href="">Chattar Sahib</Link>
-<Link href="">Chaur Sahib</Link>
-</div>
-</div>
+    <div className="dropdown1">
+        <button className="dropbtn">Courses <i className="fa fa-caret-down"></i></button>
+        <div className="dropdown-content">
+            <Link to="/Rumala">Rumala Sahib</Link>
+            <Link href="">Chattar Sahib</Link>
+            <Link href="">Chaur Sahib</Link>
+        </div>
+    </div>
 
-<div className="dropdown1">
-<button className="dropbtn">Instructors <i className="fa fa-caret-down"></i></button>
-<div className="dropdown-content">
-<Link to="/Rumala">Rumala Sahib</Link>
-<Link href="">Chattar Sahib</Link>
-<Link href="">Chaur Sahib</Link>
-</div>
-</div>
-
-<div className="dropdown1">
-<button className="dropbtn">Events <i className="fa fa-caret-down"></i></button>
-<div className="dropdown-content">
-<Link to="/Rumala">Rumala Sahib</Link>
-<Link href="">Chattar Sahib</Link>
-<Link href="">Chaur Sahib</Link>
-</div>
-</div>
-
-<div className="dropdown1">
-<button className="dropbtn">Pages <i className="fa fa-caret-down"></i></button>
-<div className="dropdown-content">
-<Link to="/Rumala">Rumala Sahib</Link>
-<Link href="">Chattar Sahib</Link>
-<Link href="">Chaur Sahib</Link>
-</div>
-</div>
-
-<div className="dropdown1">
-<button className="dropbtn">Elements <i className="fa fa-caret-down"></i></button>
-<div className="dropdown-content">
-<Link to="/Rumala">Rumala Sahib</Link>
-<Link href="">Chattar Sahib</Link>
-<Link href="">Chaur Sahib</Link>
-</div>
-</div>
+    <div className="dropdown1">
+        <button className="dropbtn">Instructors <i className="fa fa-caret-down"></i></button>
+        <div className="dropdown-content">
+            <Link to="/Rumala">Rumala Sahib</Link>
+            <Link href="">Chattar Sahib</Link>
+            <Link href="">Chaur Sahib</Link>
+        </div>
+    </div>
 </nav>
 )
 }
 
-export default NavigationBar;
+export default Navbar;

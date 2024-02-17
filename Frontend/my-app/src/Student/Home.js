e2d01c54e01c412e6192edc0ebc03ebf40d09c15
Harmeet <102729017+Harmeet03@users.noreply.github.com>
2024-02-17 18:05:33 +0530

    Aman for you for subject of student

--- Frontend/my-app/src/Student/Home.js
+++ Frontend/my-app/src/Student/Home.js
@@ -25,32 +25,65 @@ const Home = () => {
             <div className="sub">
                 <i className="fa fa-book"></i>
                 <h2> Total Subjects </h2>
-                <p></p>
+                {/* FOR BACKEND */}
+                <p> 0 </p>
             </div>
             <div className="marks">
                 <i className="fa fa-percent"></i>
                 <h2> Total Marks </h2>
-                <p></p>
+                {/* FOR BACKEND */}
+                <p> 0 </p>
             </div>
             <div className="ass">
                 <i className="fa fa-bookmark"></i>
                 <h2> Total Assignments </h2>
-                <p></p>
+                {/* FOR BACKEND */}
+                <p> 0 </p>
             </div>
             <div className="att">
                 <i className="fa fa-address-book"></i>
                 <h2> Total Attendance </h2>
-                <p></p>
+                {/* FOR BACKEND */}
+                <p> 0 </p>
             </div>
             <div className="notes">
                 <i className="fa fa-sticky-note"></i>
                 <h2> Total Notes </h2>
-                <p></p>
+                {/* FOR BACKEND */}
+                <p> 0 </p>
             </div>
             <div className="tests">
                 <i className="fa fa-book"></i>
                 <h2> Total Tests </h2>
-                <p></p>
+                {/* FOR BACKEND */}
+                <p> 0 </p>
+            </div>
+        </div>
+
+        <div id="noticeS">
+            <div className="noNoticeS">
+                <h3> No Notice for you right now. </h3>
+            </div>
+            <div className="noticeS">
+                {/* FOR BACKEND */}
+                <table>
+                    <thead>
+                        <tr>
+                            {/* <td className="date"> DATE </td>
+                            <td className="title"> TITLE / NOTICE </td> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {/* <td>12</td>
+                            <td>fddfdf</td> */}
+                        </tr>
+                        <tr>
+                            {/* <td>12</td>
+                            <td>fddfdf</td> */}
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
         </>
---
+++ Frontend/my-app/src/Student/Marks.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '../App.css';
+import { useNavigate } from "react-router-dom";
+
+const Marks = () => {
+    const navigate = useNavigate()
+    return(
+        <>
+        <head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+            <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet"></link>
+            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+            <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Oswald:wght@300&display=swap" rel="stylesheet"></link>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        </head>
+        
+        </>
+    )
+};
+
+export default Marks;
